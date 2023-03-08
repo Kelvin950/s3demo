d263d847fbc4ee9e43bcbfbe48a2e377ec374641
kelvin <denlinato@gmail.com>
2023-03-08 00:28:38 +0000

    uploading successfull

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { NextFunction , Request , Response } from 'express';
 import multer from 'multer';
+import { S3Client , PutObjectCommand } from "@aws-sdk/client-s3";
+import "dotenv/config"
 const app  = express();
 
 
@@ -7,7 +9,13 @@ const storage =  multer.memoryStorage();
 const upload =  multer({storage:storage}) ;
 
 
-upload.single("image")
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS!,
+    secretAccessKey: process.env.s3_SECRET!,
+  },
+  region:process.env.BUCKET_REGION!
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true})) ; 
@@ -18,10 +26,20 @@ app.get("/api/post/:id" , (req:Request , res:Response)=>{
 }) 
 
 
-app.post("/api/post" , (req:Request , res:Response)=>{
+app.post("/api/post" , upload.single("image") ,async (req:Request , res:Response)=>{
    
  console.log(req.body)
+console.log(req.file)
 
+const params = {
+    Bucket:process.env.BUCKET_NAME!,
+    Key:req.file?.originalname , 
+    Body:req.file?.buffer ,
+    ContentType:req.file?.mimetype
+}
+
+const command =  new PutObjectCommand(params) ; 
+ await s3.send(command)
  res.end()
 }) ;
 
